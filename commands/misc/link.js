e8089933a6c1aca33f30d033acf43e0edf5d0a5d
robinsongz <mrrobinsung@gmail.com>
2019-05-21 15:43:05 -0700

    added !link command

---
+++ commands/misc/link.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+
+module.exports.run = async (bot, message) => {
+
+    const guildConf = enmap.ensure(message.guild.id, defaultSettings);
+
+    if (!message.guild || message.author.bot) return;
+  
+    if (message.content.indexOf(guildConf.prefix) !== 0) return;
+        
+        // Link skills data
+
+        return message.reply(`https://imgur.com/y76SKzq`)
+        
+       
+    }
+
+
+module.exports.help = {
+    name: 'link'
+}
