4001d796f13da74b1bcc20fd9f9dc9411ea9d664
robinsongz <mrrobinsung@gmail.com>
2019-06-25 17:11:39 -0700

    added exalt armors data

--- commands/forge/exalt.js
+++ commands/forge/exalt.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 
-module.exports.run = async (bot, message) => {
+module.exports.run = async (bot, message, args) => {
 
     const guildConf = enmap.ensure(message.guild.id, defaultSettings);
 
@@ -10,9 +10,23 @@ module.exports.run = async (bot, message) => {
     if (message.content.indexOf(guildConf.prefix) !== 0) return;
         
     // exalt data
-        
-    return message.reply(`https://imgur.com/a/Qp1eu9B`);
+    const [prop] = args;
 
+    if (prop === 'weapon') {
+        return message.channel.send(`https://imgur.com/a/Qp1eu9B`);
+    }
+    
+    else if (prop === 'armor1') {
+        return message.channel.send(`https://imgur.com/s8YhEE7`);
+    }
+
+    else if (prop === 'armor2') {
+        return message.channel.send(`https://imgur.com/4Nx7A5r`);
+    }
+
+    else {
+        return message.channel.send(`Please type **!exalt weapon**, **!exalt armor1**, or **!exalt armor2**`)
+    }
 
     };
 
